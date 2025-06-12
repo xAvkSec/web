3b063b15692f77d6529744a20be3b2b6ca723d32
phims <149851683+phims403@users.noreply.github.com>
2025-06-13 06:41:18 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,3 +1,4 @@
 <?php
-file_put_contents("log.txt", $_SERVER['REMOTE_ADDR'] . " - " . $_GET['c'] . "\n", FILE_APPEND);
+file_put_contents("log.txt", date("Y-m-d H:i:s") . " - " . $_SERVER['REMOTE_ADDR'] . " - " . $_SERVER['REQUEST_URI'] . " - " . file_get_contents("php://input") . "\n", FILE_APPEND);
+echo "OK";
 ?>
